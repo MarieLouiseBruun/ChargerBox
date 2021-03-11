fd0badabbe9f87817e0d1aab2f03ad9c07aec6c5
MarieLouiseBruun <60734405+MarieLouiseBruun@users.noreply.github.com>
2021-03-11 13:32:16 +0100

    Add project files.

---
+++ ChargerBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChargerBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ChargingStation/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChargingStation
+{
+    public class Class1
+    {
+    }
+}
