7d59507d4e0b576e7d71cca0a9433f8e2c1979b9
MarieLouiseBruun <60734405+MarieLouiseBruun@users.noreply.github.com>
2021-03-11 14:50:19 +0100

    all FRABJs Class's comitted

--- ChargingStation/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ChargingStation
-{
-    public class Class1
-    {
-    }
-}
---
+++ ChargingStation/CurrentEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargingStation
+{
+    public class CurrentEventArgs : EventArgs
+    {
+        // Value in mA (milliAmpere)
+        public double Current { set; get; }
+    }
+}
