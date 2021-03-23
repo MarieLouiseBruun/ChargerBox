ae28393c19153b9a1d4568b80bf555123ac6635d
Dina Bæk Andersen <au648834@uni.au.dk>
2021-03-23 15:59:57 +0100

    Test DoorSimulator and RfidReaderSimulator

---
+++ ChargerUnitTest/TestDoorSimulator.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ChargingStation;
+
+namespace ChargerUnitTest
+{
+    [TestFixture]
+    public class TestDoorSimulator
+    {
+        private DoorSimulator _uut;
+        private DoorEventArgs _eventArgs; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventArgs = null; 
+
+            _uut = new DoorSimulator();
+
+            _uut.IsOpenValueEvent +=
+                (o, args) =>
+                {
+                    _eventArgs = args;
+                };
+        }
+
+        [Test]
+        public void DoorStateChanged_IsOpenSetToTrue_EventFired()
+        {
+            _uut.OnDoorOpen();
+            Assert.That(_eventArgs, Is.Not.Null);
+        }
+
+        [Test]
+        public void DoorStateChanged_IsOpenSetToFalse_EventFired()
+        {
+            _uut.OnDoorClose();
+            Assert.That(_eventArgs, Is.Not.Null);
+        }
+
+        [Test]
+        public void DoorStateChanged_IsOpenSetToTrue_CorrectNewStateReceived()
+        {
+            _uut.OnDoorOpen();
+            Assert.That(_eventArgs.IsOpen, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void DoorStateChanged_IsOpenSetToFalse_CorrectNewStateReceived()
+        {
+            _uut.OnDoorClose();
+            Assert.That(_eventArgs.IsOpen, Is.EqualTo(false));
+        }
+
+        //Test Lock/Unlock - Console.Writeline()
+    }
+}
---
+++ ChargerUnitTest/TestRfidReaderSimulator.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ChargingStation;
+
+namespace ChargerUnitTest
+{
+    [TestFixture]
+    public class TestRfidReaderSimulator
+    {
+        private RfidReaderSimulator _uut;
+        private RfidEventArgs _eventArgs; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventArgs = null; 
+
+            _uut = new RfidReaderSimulator();
+
+            _uut.RfidEvent +=
+                (o, args) =>
+                {
+                    _eventArgs = args;
+                };
+        }
+
+        [Test]
+        public void RfidChanged_IdSetToNewValue_EventFired()
+        {
+            _uut.OnRfidRead(100);
+            Assert.That(_eventArgs, Is.Not.Null);
+        }
+
+        [Test]
+        public void RfidChanged_IdSetToNewValue_CorrectNewIdReceived()
+        {
+            _uut.OnRfidRead(100);
+            Assert.That(_eventArgs.RfID, Is.EqualTo(100));
+        }
+
+
+
+
+    }
+}
