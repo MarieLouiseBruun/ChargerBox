0b3d3dba2ccad86e893c9c1024aaba18ee47fb01
Sarah Amalie Vestmark Pind <au554980@uni.au.dk>
2021-03-24 08:36:45 +0100

    Added TestStationControl

---
+++ ChargerUnitTest/TestStationControl.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ChargerBox;
+
+namespace ChargerUnitTest
+{
+    [TestFixture]
+    public class TestStationControl
+    {
+        private StationControl _uut;
+        [SetUp]
+        public void Setup()
+        {
+           // _uut = new StationControl();
+        }
+
+        [Test] public void hej()
+        {
+
+        }
+        
+    }
+}
