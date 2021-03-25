d061f405122369aeabac8a3c9f76a9b9a056bd15
MarieLouiseBruun <60734405+MarieLouiseBruun@users.noreply.github.com>
2021-03-25 16:57:42 +0100

    display test færdiggjort

---
+++ ChargerUnitTest/TestDisplay.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ChargingStation;
+using ChargingStation.Interfaces;
+
+namespace ChargerUnitTest
+{
+    [TestFixture]
+    public class TestDisplay
+    {
+        private IDisplay _uut;
+        private StringWriter _stringWriter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+            _stringWriter = new StringWriter();
+            System.Console.SetOut(_stringWriter);
+        }
+
+       
+
+     
+        [Test]
+        public void PrintMessageOnDisplay()
+        {
+            _uut.Print("Hej Frank - god påske");
+            var text = _stringWriter.ToString();
+            Assert.AreEqual("Hej Frank - god påske\r\n", text);
+        }
+
+    }
+}
